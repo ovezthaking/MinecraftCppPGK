76c85a5b881813f16d63537a2ad21b07ba2140d3
ovezthaking <oliwerx12@gmail.com>
2025-01-23 02:25:32 +0100

    small update

--- Minecraft/src/Chunk.h
+++ Minecraft/src/Chunk.h
@@ -15,6 +15,7 @@
 #include <optional>
 
 
+
 /** Set of cubes representing fixed size chunk.
  * We associate Width dimension with X axis, Height dimension with Y axis and Depth with Z axis.
  * Order of dimensions is as follow: depth, width, heigh.
@@ -50,11 +51,13 @@ public:
 private:
     size_t CoordsToIndex(size_t depth, size_t width, size_t height) const;
     void UpdateVisibility();
+    void UpdateBlockVisibility(size_t depth, size_t width, size_t height);
 
     CubePalette& m_palette;
     FlattenData_t m_data;
     glm::vec2 m_origin;
     AABB m_aabb;
+    std::vector<size_t> m_visibleBlocks;
 };
 
 template <uint8_t Depth, uint8_t Width, uint8_t Height>
@@ -103,19 +106,15 @@ template <uint8_t Depth, uint8_t Width, uint8_t Height>
 inline void Chunk<Depth, Width, Height>::Draw(ShaderProgram& shader) const {
     shader.Use();
 
-    for (size_t z = 0; z < Depth; ++z) {
-        for (size_t x = 0; x < Width; ++x) {
-            for (size_t y = 0; y < Height; ++y) {
-                size_t index = CoordsToIndex(z, x, y);
+    for (size_t index : m_visibleBlocks) {
+        size_t z = index % Depth;
+        size_t x = (index / Depth) % Width;
+        size_t y = index / (Depth * Width);
 
-                if (m_data[index].m_isVisible) {
-                    glm::mat4 model = glm::translate(
-                        glm::mat4(1.0f), glm::vec3(m_origin.x + x, y, m_origin.y + z));
-                    shader.SetMat4("model", model);
-                    m_palette.LookUp(m_data[index].m_type).Draw();
-                }
-            }
-        }
+        glm::mat4 model = glm::translate(
+            glm::mat4(1.0f), glm::vec3(m_origin.x + x, y, m_origin.y + z));
+        shader.SetMat4("model", model);
+        m_palette.LookUp(m_data[index].m_type).Draw();
     }
 }
 
@@ -131,38 +130,33 @@ inline Ray::HitType Chunk<Depth, Width, Height>::Hit(const Ray& ray, Ray::time_t
     Ray::time_t closestTime = max;
     bool hitDetected = false;
 
-    for (size_t z = 0; z < Depth; ++z) {
-        for (size_t x = 0; x < Width; ++x) {
-            for (size_t y = 0; y < Height; ++y) {
-                size_t index = CoordsToIndex(z, x, y);
-                if (!m_data[index].m_isVisible || m_data[index].m_type == Cube::Type::None) {
-                    continue;
-                }
+    for (size_t index : m_visibleBlocks) {
+        size_t z = index % Depth;
+        size_t x = (index / Depth) % Width;
+        size_t y = index / (Depth * Width);
 
-                glm::vec3 cubeMin = chunkOffset + glm::vec3(x, y, z);
-                glm::vec3 cubeMax = cubeMin + glm::vec3(1.0f); // Cubes are 1x1x1
-                AABB cubeAABB(cubeMin, cubeMax);
+        glm::vec3 cubeMin = chunkOffset + glm::vec3(x, y, z);
+        glm::vec3 cubeMax = cubeMin + glm::vec3(1.0f); // Cubes are 1x1x1
+        AABB cubeAABB(cubeMin, cubeMax);
 
-                AABB::HitRecord cubeRecord;
-                if (cubeAABB.Hit(ray, min, closestTime, cubeRecord) == Ray::HitType::Hit) {
-                    closestTime = cubeRecord.m_time;
-                    record.m_cubeIndex = glm::ivec3(x, y, z);
+        AABB::HitRecord cubeRecord;
+        if (cubeAABB.Hit(ray, min, closestTime, cubeRecord) == Ray::HitType::Hit) {
+            closestTime = cubeRecord.m_time;
+            record.m_cubeIndex = glm::ivec3(x, y, z);
 
-                    glm::ivec3 neighborOffset = glm::ivec3(0);
-                    if (cubeRecord.m_axis == AABB::Axis::x) {
-                        neighborOffset.x = (ray.Direction().x > 0) ? -1 : 1;
-                    }
-                    else if (cubeRecord.m_axis == AABB::Axis::y) {
-                        neighborOffset.y = (ray.Direction().y > 0) ? -1 : 1;
-                    }
-                    else if (cubeRecord.m_axis == AABB::Axis::z) {
-                        neighborOffset.z = (ray.Direction().z > 0) ? -1 : 1;
-                    }
-                    record.m_neighbourIndex = record.m_cubeIndex + neighborOffset;
-
-                    hitDetected = true;
-                }
+            glm::ivec3 neighborOffset = glm::ivec3(0);
+            if (cubeRecord.m_axis == AABB::Axis::x) {
+                neighborOffset.x = (ray.Direction().x > 0) ? -1 : 1;
             }
+            else if (cubeRecord.m_axis == AABB::Axis::y) {
+                neighborOffset.y = (ray.Direction().y > 0) ? -1 : 1;
+            }
+            else if (cubeRecord.m_axis == AABB::Axis::z) {
+                neighborOffset.z = (ray.Direction().z > 0) ? -1 : 1;
+            }
+            record.m_neighbourIndex = record.m_cubeIndex + neighborOffset;
+
+            hitDetected = true;
         }
     }
 
@@ -177,10 +171,9 @@ inline bool Chunk<Depth, Width, Height>::RemoveBlock(uint8_t width, uint8_t heig
         return false; // Blok ju¿ nie istnieje
     }
     m_data[index].m_type = Cube::Type::None; // Ustaw typ na None
-    UpdateVisibility(); // Zaktualizuj widocznoœæa
+    UpdateBlockVisibility(depth, width, height); // Zaktualizuj widocznoœæ s¹siadów
     return true; // Blok zosta³ usuniêty
 }
-
 
 
 template <uint8_t Depth, uint8_t Width, uint8_t Height>
@@ -191,7 +184,7 @@ inline bool Chunk<Depth, Width, Height>::PlaceBlock(uint8_t width, uint8_t heigh
     }
 
     m_data[index].m_type = type;
-    UpdateVisibility();
+    UpdateBlockVisibility(depth, width, height); // Zaktualizuj widocznoœæ s¹siadów
     return true;
 }
 
@@ -205,6 +198,8 @@ inline size_t Chunk<Depth, Width, Height>::CoordsToIndex(size_t depth,
 
 template <uint8_t Depth, uint8_t Width, uint8_t Height>
 inline void Chunk<Depth, Width, Height>::UpdateVisibility() {
+    m_visibleBlocks.clear();
+
     for (size_t z = 0; z < Depth; ++z) {
         for (size_t x = 0; x < Width; ++x) {
             for (size_t y = 0; y < Height; ++y) {
@@ -223,7 +218,21 @@ inline void Chunk<Depth, Width, Height>::UpdateVisibility() {
                 if (y == Height - 1 || m_data[CoordsToIndex(z, x, y + 1)].m_type == Cube::Type::None) isVisible = true;
 
                 m_data[index].m_isVisible = isVisible;
+                if (isVisible) {
+                    m_visibleBlocks.push_back(index);
+                }
             }
         }
     }
+}
+
+template <uint8_t Depth, uint8_t Width, uint8_t Height>
+inline void Chunk<Depth, Width, Height>::UpdateBlockVisibility(size_t depth, size_t width, size_t height) {
+    auto updateNeighbor = [&](size_t d, size_t w, size_t h) {
+        if (d < Depth && w < Width && h < Height) {
+            UpdateVisibility(); 
+        }
+        };
+
+    updateNeighbor(depth, width, height);
 }
